0c7e9acf773f4e1108e564108cca6441bc3ad1a9
Chaemin-L <chaem12@kookmin.ac.kr>
2024-11-14 15:08:34 +0900

    feat: link, item 슬라이더 구현

---
+++ src/components/item-slider.tsx
@@ -0,0 +1,52 @@
+import Progressbar from "./progressbar";
+
+interface ItemType {
+  title: string;
+  category: string;
+  percent: number;
+  image: string;
+}
+
+interface Props {
+  items: ItemType[];
+}
+
+export default function ItemSlider({ items }: Props) {
+  return (
+    <div className="overflow-auto">
+      <ul className="flex overflow-auto gap-3 w-fit">
+        {items.map(({ title, category, percent, image }, idx) => (
+          <li key={idx} className="w-40 select-none ">
+            <img
+              src={image}
+              className="w-40 aspect-[1.2/1] object-cover rounded-t-xl"
+            />
+            <div className="flex flex-col gap-2 bg-shadow-800 rounded-b-xl px-3 py-4">
+              <span className="text-bd4 font-normal">{category}</span>
+              <h1 className="text-bd4 line-clamp-1">{title}</h1>
+              <Progressbar isShort percent={percent} />
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+    // <Swiper
+    //   modules={[FreeMode]}
+    //   spaceBetween={12}
+    //   slidesPerView={2}
+    //   freeMode={true}
+    //   style={{ margin: -20, padding: 20 }}
+    // >
+    //   {items.map(({ title, category, percent, image }) => (
+    //     <SwiperSlide className="select-none cursor-grab active:cursor-grabbing">
+    //       <img src={image} className="rounded-t-xl" />
+    //       <div className="flex flex-col gap-2 bg-shadow-800 rounded-b-xl px-3 py-4">
+    //         <span className="text-bd4 font-normal">{category}</span>
+    //         <h1 className="text-bd4 line-clamp-1">{title}</h1>
+    //         <Progressbar percent={percent} />
+    //       </div>
+    //     </SwiperSlide>
+    //   ))}
+    // </Swiper>
+  );
+}
---
+++ src/components/link-slider.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination } from "swiper/modules";
+import "swiper/css/pagination";
+import "swiper/css";
+import "@/styles/swiper.css";
+import Link from "next/link";
+import { PATH } from "@/lib/paths";
+
+interface LinkType {
+  title: string;
+  description: string;
+}
+
+interface Props {
+  links: LinkType[];
+}
+
+export default function LinkSlider({ links }: Props) {
+  return (
+    <Swiper
+      modules={[Autoplay, Pagination]}
+      pagination={{ dynamicBullets: true, clickable: true }}
+      spaceBetween={20}
+      slidesPerView={1}
+      autoplay={{
+        delay: 3000,
+        stopOnLastSlide: false,
+        pauseOnMouseEnter: true,
+      }}
+      style={{ margin: -20, padding: 20 }}
+    >
+      {links.map(({ title, description }) => (
+        <SwiperSlide className="select-none cursor-grab active:cursor-grabbing space-y-1">
+          <Link href={PATH.FUNDRAISINGS}>
+            <h1 className="text-t4">{title}</h1>
+            <p className="text-bd3"> {description}</p>
+          </Link>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+}
