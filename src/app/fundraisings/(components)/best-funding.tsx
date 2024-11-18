0e66de6a2179c15362ec7c7ec761e85b7da1ecfb
Chaemin-L <chaem12@kookmin.ac.kr>
2024-11-18 18:06:58 +0900

    fix: 기부탭 페이지 스타일링 수정

--- src/app/fundraisings/(components)/best-funding.tsx
+++ src/app/fundraisings/(components)/best-funding.tsx
@@ -9,7 +9,7 @@ export default function BestFunding() {
         <br />
         숲을 만날 수 있기를
       </div>
-      <div className="bg-gray-200 w-full h-[156px] rounded-2xl" />
+      <div className="bg-gray-200 w-full  aspect-[2/1.2] rounded-2xl" />
       <FundingStatus />
     </Link>
   );
