c3cac5cd7612c92431f85bc5d2f3931c00b0117d
Chaemin-L <chaem12@kookmin.ac.kr>
2024-11-18 13:25:34 +0900

    feat: 공통 컴포넌트 checkbox, searchbar 구현

---
+++ src/app/(_components)/checkbox.tsx
@@ -0,0 +1,29 @@
+import { InputHTMLAttributes, MouseEventHandler } from "react";
+import Image from "next/image";
+
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+}
+
+export default function Checkbox({ label = "", ...props }: Props) {
+  return (
+    <label className="flex gap-7 py-5">
+      <input type="checkbox" {...props} className="peer hidden" />
+      <Image
+        src="/icons/checkbox-x.svg"
+        width={16}
+        height={16}
+        alt="체크박스 해제"
+        className="peer-checked:hidden"
+      />
+      <Image
+        src="/icons/checkbox-o.svg"
+        width={16}
+        height={16}
+        alt="체크박스 해제"
+        className="hidden peer-checked:block"
+      />
+      <span className="text-bd1">{label}</span>
+    </label>
+  );
+}
---
+++ src/app/(_components)/searchbar.tsx
@@ -0,0 +1,25 @@
+"use client";
+import Image from "next/image";
+import { FormEventHandler, MouseEventHandler } from "react";
+
+interface Props {
+  onSubmit?: FormEventHandler<HTMLFormElement>;
+}
+
+export default function SearchBar({ onSubmit }: Props) {
+  return (
+    <form
+      className="flex gap-3 items-center bg-shadow-800 rounded-full px-6 py-3"
+      onSubmit={onSubmit}
+    >
+      <input
+        type="text"
+        className="flex-1 bg-transparent focus:outline-0 text-t4"
+        autoFocus
+      />
+      <button>
+        <Image src="/icons/search.svg" width={16} height={16} alt="검색" />
+      </button>
+    </form>
+  );
+}
