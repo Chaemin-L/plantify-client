137340be3bf9ee5df7f0721e8982aeb600089033
Chaemin-L <chaem12@kookmin.ac.kr>
2024-11-21 09:56:36 +0900

    fix: 은행 이름명 변경

--- src/lib/_shared/bankInfo.ts
+++ src/lib/_shared/bankInfo.ts
@@ -1,13 +1,13 @@
 import { BankIdValue } from "@/types/bank";
 
 export const bankList: BankIdValue[] = [
-  { id: "toss", name: "토스" },
-  { id: "kakao", name: "카카오" },
+  { id: "toss", name: "토스뱅크" },
+  { id: "kakao", name: "카카오뱅크" },
   { id: "hana", name: "하나" },
   { id: "shinhan", name: "신한" },
   { id: "kb", name: "국민" },
   { id: "woori", name: "우리" },
-  { id: "ibk", name: "IBK" },
+  { id: "ibk", name: "IBK기업" },
   { id: "kbank", name: "케이뱅크" },
   { id: "nonghyup", name: "농협" },
   { id: "sc", name: "SC제일" },
