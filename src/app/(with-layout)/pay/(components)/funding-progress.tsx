a425d311c51a559a286a40e1bb79cb8486c298be
Chaemin-L <chaem12@kookmin.ac.kr>
2024-12-13 02:31:20 +0900

    feat: 내 펀딩 empty case 처리

--- src/app/(with-layout)/pay/(components)/funding-progress.tsx
+++ src/app/(with-layout)/pay/(components)/funding-progress.tsx
@@ -1,22 +1,25 @@
 "use client";
 import FundingStatus from "@/app/(_components)/funding-status";
 import { PATH } from "@/lib/_shared/paths";
+import { FundingType } from "@/types/api/funding";
 import Link from "next/link";
 
-const dummy = Array(3)
-  .fill({
-    title: "우리 아이가 다시 일어설 수만 있다면",
-    percent: 47,
-    targetAmount: 3000000,
-    organizationName: "천안시종합사회복지관",
-  })
-  .map((item, idx) => ({ ...item, id: idx }));
+const dummy: FundingType[] = [];
+// Array(3)
+//   .fill({
+//     title: "우리 아이가 다시 일어설 수만 있다면",
+//     percent: 47,
+//     targetAmount: 3000000,
+//     organizationName: "천안시종합사회복지관",
+//   })
+//   .map((item, idx) => ({ ...item, id: idx }));
 
 export default function FundingProgress() {
+  const isEmpty = dummy.length === 0;
   return (
     <>
       <div className="flex justify-between px-2">
-        <h2 className="card-title select-none">펀딩현황</h2>
+        <h2 className="card-title select-none">내 펀딩</h2>
         <Link
           href={PATH.FUNDING_MY}
           className=" ml-auto w-fit text-bd2 hover:opacity-80"
@@ -24,23 +27,26 @@ export default function FundingProgress() {
           전체보기 &gt;
         </Link>
       </div>
-      <div className="card cursor-pointer flex flex-col gap-8">
+      <div className="card  flex flex-col gap-8">
         <div
           className=" bg-shadow-800 rounded-2xl text-center  text-white flex flex-col gap-2"
           onClick={(e) => e.stopPropagation()}
         >
           {/** TODO: 마이 펀딩 리스트 라우팅 경로로 변경 */}
-
-          <ul className="w-full flex flex-col gap-5">
-            {dummy.map((item, idx) => (
-              <li
-                key={idx}
-                className="h-fit w-full hover:bg-shadow-700 rounded-2xl transition-colors"
-              >
-                <FundingItem {...item} />
-              </li>
-            ))}
-          </ul>
+          {isEmpty ? (
+            <p>진행중인 펀딩이 존재하지 않습니다</p>
+          ) : (
+            <ul className="w-full flex flex-col gap-5 cursor-pointer">
+              {dummy.map((item, idx) => (
+                <li
+                  key={idx}
+                  className="h-fit w-full hover:bg-shadow-700 rounded-2xl transition-colors "
+                >
+                  <FundingItem {...item} />
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
       </div>
     </>
@@ -48,14 +54,14 @@ export default function FundingProgress() {
 }
 
 interface Props {
-  id: string;
+  fundingId: string;
   title: string;
   percent: number;
   targetAmount: number;
-  organizationName: string;
+  organizationName?: string;
 }
 const FundingItem = (props: Props) => {
-  const { id, title, percent, targetAmount, organizationName } = props;
+  const { title, percent, targetAmount, organizationName } = props;
 
   return (
     <Link
@@ -65,7 +71,6 @@ const FundingItem = (props: Props) => {
       <FundingStatus
         size="reactive"
         showProgress
-        id={id}
         leftTop={title}
         percent={percent}
         targetAmount={targetAmount}
