5fcee886e8dfbd1f2bfce29c1dd5cc1822820bdc
Chaemin-L <chaem12@kookmin.ac.kr>
2024-12-13 06:13:42 +0900

    chore: 카카오 로그인 URL 공통화

--- src/app/intro/plantify-cta.tsx
+++ src/app/intro/plantify-cta.tsx
@@ -1,13 +1,8 @@
+import { kakaoAuthURL } from "@/config/api";
 import Link from "next/link";
 import FeatureLayout from "./(components)/feature-layout";
 
-const REDIRECT_URI = process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI;
-const CLIENT_ID = process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID;
-const CLIENT_SECRET = process.env.NEXT_PUBLIC_KAKAO_CLIENT_SECRET;
-
 export default function PlantiCTA() {
-  const kakaoAuthURL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&redirect_uri=${REDIRECT_URI}&response_type=code`;
-
   return (
     <FeatureLayout
       title="PlantiFy+"
--- src/config/api.ts
+++ src/config/api.ts
@@ -2,6 +2,10 @@ export const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 export const CHAT_BASE_URL = process.env.NEXT_PUBLIC_CHAT_BASE_URL;
 export const ML_BASE_URL = process.env.NEXT_PUBLIC_ML_API_BASE_URL;
 
+const REDIRECT_URI = process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI;
+const CLIENT_ID = process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID;
+const CLIENT_SECRET = process.env.NEXT_PUBLIC_KAKAO_CLIENT_SECRET;
+
 export const API_ENDPOINTS = {
   AUTH: `${BASE_URL}/v1/auth`,
   PAY: `${BASE_URL}/v1/pay`,
@@ -11,3 +15,5 @@ export const API_ENDPOINTS = {
   CASH: `${BASE_URL}/v1/cash`,
   CARD: `${BASE_URL}/v1/recommend/cards`,
 };
+
+export const kakaoAuthURL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&redirect_uri=${REDIRECT_URI}&response_type=code`;
