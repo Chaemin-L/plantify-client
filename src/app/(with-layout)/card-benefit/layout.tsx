50612ef8137dedcc7ff0dbfd5662a090232e6b3e
Chaemin-L <chaem12@kookmin.ac.kr>
2024-12-15 16:56:14 +0900

    chore: 메타데이터 변수명 오타 수정

--- src/app/(with-layout)/card-benefit/layout.tsx
+++ src/app/(with-layout)/card-benefit/layout.tsx
@@ -1,6 +1,6 @@
 import { Metadata } from "next";
 
-export const meta: Metadata = {
+export const metadata: Metadata = {
   title: "Plantify - 더 큰 카드 혜택 추천",
   description: "사용처에서 혜택이 큰 카드를 추천해요",
   keywords: ["plantify", "card", "benefit", "recommend"],
