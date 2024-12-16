60dac13f1905f9bb96c8925c6a34d8542d81c6b3
Chaemin-L <chaem12@kookmin.ac.kr>
2024-12-16 18:21:36 +0900

    fix: 스플래시 로고 크기 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Page() {
       className={`flex justify-center items-center h-full ${MONEYGRAPHY.variable} ${MONEYGRAPHY.className}`}
     >
       <div className="text-[36px] font-bold animate-[fadeIn_1s] select-none flex justify-center items-center">
-        <img src="/icons/logo.png" className="inline w-12-mx-0.5" />
+        <img src="/icons/logo.png" className="inline w-40 -mx-0.5" />
       </div>
     </div>
   );
