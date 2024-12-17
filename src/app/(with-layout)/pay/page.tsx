d8eb03b4ffc07aa96eeb4e7a2f31fa69deceecda
Chaemin-L <chaem12@kookmin.ac.kr>
2024-12-17 18:48:58 +0900

    fix: 메인 페이지 카드 혜택 섹션에 더미 이미지 추가

--- src/app/(with-layout)/pay/page.tsx
+++ src/app/(with-layout)/pay/page.tsx
@@ -62,13 +62,69 @@ export default function HomePage() {
   );
 }
 
-const listData: GetMyCardRes = Array(6).fill({
-  card_id: 0,
-  myCard_id: 0,
-  card: {
-    card_name: "현대",
-    card_image:
-      "https://d1c5n4ri2guedi.cloudfront.net/card/13/card_img/28201/13card.png",
-    benefit_point: 10,
+const listData: GetMyCardRes = [
+  {
+    card: {
+      card_name: "현대",
+      card_image:
+        "https://d1c5n4ri2guedi.cloudfront.net/card/13/card_img/28201/13card.png",
+      benefit_point: 10,
+    },
   },
+
+  {
+    card: {
+      card_name: "BC 바로 리워드 플러스",
+      card_image:
+        "https://d1c5n4ri2guedi.cloudfront.net/card/2347/card_img/32524/2347card.png",
+      benefit_point: 10,
+    },
+  },
+
+  {
+    card: {
+      card_name: "토스 신용카드",
+      card_image:
+        "https://d1c5n4ri2guedi.cloudfront.net/card/589/card_img/21346/589card.png",
+      benefit_point: 10,
+    },
+  },
+
+  {
+    card: {
+      card_name: "카카오뱅크 개인사업자 삼성카드",
+      card_image:
+        "https://d1c5n4ri2guedi.cloudfront.net/card/2420/card_img/28101/2420card.png",
+      benefit_point: 10,
+    },
+  },
+
+  {
+    card: {
+      card_name: "모니모A",
+      card_image:
+        "https://d1c5n4ri2guedi.cloudfront.net/card/2631/card_img/31783/2631card.png",
+      benefit_point: 10,
+    },
+  },
+
+  {
+    card: {
+      card_name: "컬리카드",
+      card_image:
+        "https://d1c5n4ri2guedi.cloudfront.net/card/2518/card_img/28798/2518card.png",
+      benefit_point: 10,
+    },
+  },
+
+  {
+    card: {
+      card_name: "T우주 신한카드",
+      card_image:
+        "https://d1c5n4ri2guedi.cloudfront.net/card/2302/card_img/23140/2302card.png",
+      benefit_point: 10,
+    },
+  },
+].map((data, idx) => {
+  return { ...data, card_id: idx, myCard_id: idx };
 });
