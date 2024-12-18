c178febb88c7f651639d724bd89264c996b87d79
Chaemin-L <chaem12@kookmin.ac.kr>
2024-12-19 07:54:29 +0900

    fix: 잘못 전달된 인자 제거

--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -1,18 +1,12 @@
-"use client";
-
 import { PATH } from "@/lib/_shared/paths";
 import Link from "next/link";
 
-export default function Error({
-  error,
-}: {
-  error: Error & { digest?: string };
-}) {
+export function Error() {
   return (
     <div className="flex justify-around items-center flex-col h-full">
       <div className="flex flex-col text-center gap-4">
-        <h1 className="text-t1">{error.name}</h1>
-        <p className="text-t2">{error.message}</p>
+        <h1 className="text-t1">Error</h1>
+        <p className="text-t2">에러가 발생했습니다</p>
       </div>
       <Link
         href={PATH.HOME}
